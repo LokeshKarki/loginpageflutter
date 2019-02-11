22c1cc20986741a61c751b0de28ba909e93293fd
Lokesh Karki <LKSHKARKI@GMAIL.COM>
2019-02-11 21:41:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: null,
         home: new LoginPage(),
         theme: new ThemeData(primarySwatch: Colors.blue));
   }
